7802d83bfc4d2d60fdbcdfdc7cb6d2d00b6fe24a
Shaung Cheng <shaungc@umich.edu>
2018-10-17 17:48:19 -0400

    add expoer to bash

--- install-env-lrc-and-run.sh
+++ install-env-lrc-and-run.sh
@@ -1,7 +1,7 @@
 ##### Env Setup
 cd $HOME && \
 git clone https://github.com/mxcl/homebrew.git
-echo 'export PATH=$HOME/homebrew/bin:$PATH' >> ~/.bashrc
+echo 'export PATH=$HOME/homebrew/bin:$PATH' >> ~/.bash_profile
 brew update # in order to proceed, mxay need to do brew update. See instruction prompted
 
 xcode-select --install
